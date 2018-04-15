a9a136577fc27905b78e4581cb4e4679a0fb8044
karmen-chan <719152930@qq.com>
2018-04-15 14:20:35 +0800

    create project

---
+++ views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from hdfs import *
+import json
+import os
+import cv2
+
+human_photo_path = "/FaceMatching_on_Spark/Results/human_photo.png"
+cat_photo_path = "/FaceMatching_on_Spark/Results/cat_photo.png"
+human_haar_cascade = "/FaceMatching_on_Spark/HaarCascade/classify/haarcascade_frontalface_default.xml"
+shell_path = "/var/www/html/FaceMatching_on_spark/script/CalculateSimilarity.sh"
+
+
+client = Client("http://student62:50070")
+with client.read(human_haar_cascade) as reader:
+    faceCascade_human = cv2.CascadeClassifier(reader)
+
+
+# function of detecting human face
+# parameter specification
+#   human_path:         path to the human photo which is newly uploaded
+# return specification
+#   return 1: success
+#   return 0: fail to detect human face
+def detect_human(human_path):
+    img = cv2.imread(human_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade_human.detectMultiScale(
+        gray,
+        scaleFactor=1.14,
+        minNeighbors=5,
+        minSize=(5, 5),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    if str(faces) == "()":
+        return 0
+    else:
+        return 1
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def upload(request):
+    if request.method == "POST":
+        ret = {'status': 'fail', 'data': {'human_photo': None, 'cat_photo': None}, 'error': None}
+
+        pic_file = request.FILES.get("photo")       # get photo
+        file_path = os.path.join("static/image", pic_file.name)
+        f = open(file_path, mode="wb")
+        for chunk in pic_file.chunks():
+            f.write(chunk)
+        f.close()
+        if detect_human(file_path) == 0:
+            ret['status'] = 'fail'
+            ret['error'] = "Failed to detect the human face."
+        else:
+            client.upload(human_photo_path, pic_file, overwrite=True)
+
+            result = os.system(shell_path)     # run shell
+
+            if result == 0:
+                ret['status'] = 'success'
+                ret['data']['human_photo'] = human_photo_path
+                ret['data']['human_photo'] = cat_photo_path
+            else:
+                ret['error'] = "Failed to calculate the similarity."
+
+        return HttpResponse(json.dumps(ret), content_type='application/json')
+
+
