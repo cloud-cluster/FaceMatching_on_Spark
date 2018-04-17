d72da20c7aa1529b00fe6af442dae5956f18dce5
karmen-chan <719152930@qq.com>
2018-04-17 23:30:23 +0800

    try

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ import json
 import cv2
 import sys
 import os
+import calculate_similarity as rs
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -76,4 +77,4 @@ def upload(request):
 
 def test(request):
     # os.system("/opt/spark-2.2.1-bin-hadoop2.7/bin/spark-submit --master=yarn /var/www/html/FaceMatching_on_Spark/calculate_similarity.py")
-    run_spark()
+    rs.run_spark()
