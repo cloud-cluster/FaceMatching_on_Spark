675f1188252a33876a82c63bf67712ef6b1bf8a7
karmen-chan <719152930@qq.com>
2018-04-17 20:52:43 +0800

    fix problem of running spark

--- views.py
+++ views.py
@@ -57,6 +57,8 @@ def upload(request):
         else:
             client.upload(human_photo_path, file_path, overwrite=True)
 
+            conf = SparkConf().setMaster("local").setAppName("Success")
+            sc = SparkContext(conf=conf)
             result = os.system("sudo /opt/spark-2.2.1-bin-hadoop2.7/bin/spark-submit --master=yarn --driver-memory 7168m --executor-memory 4G /var/www/html/FaceMatching_on_Spark/calculate_similarity.py")     # run shell
 
             if result == 0:
